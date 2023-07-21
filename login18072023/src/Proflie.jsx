c36be61aacd1e513b091d6127fa702afb46579ef
juon <juonjang@gmail.com>
2023-07-21 15:42:11 +0700

    login-API successful

--- login18072023/src/Proflie.jsx
+++ login18072023/src/Proflie.jsx
@@ -2,6 +2,19 @@ import { useState, useEffect} from 'react'
 import { useNavigate } from 'react-router-dom';
 import Swal from 'sweetalert2'
 import withReactContent from 'sweetalert2-react-content'
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 
 export default function Profile() {
@@ -53,6 +66,7 @@ export default function Profile() {
     return (<div>Loading...ไม่ได้</div>);
   }else{
     return (
+      
       <div>
        <div>user : {user.loginname}</div>
        <div>name : {user.name}</div>
